f931f8b0b2a795a124c9974ac12c7e649ff935ad
guilsk <guilherme.gmdp@gmail.com>
2025-07-22 00:19:11 -0300

    First commit

---
+++ server/RPGSheetManager/RPGSheetManager.API/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace RPGSheetManager.API {
+    public class Program {
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+                    options.Audience = builder.Configuration["Auth0:Audience"];
+                });
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "RPGSheetManager.API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme {
+                    Name = "Authorization",
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Description = "Bearer {seu_token}"
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment()) {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "RPGSheetManager.API v1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ server/RPGSheetManager/RPGSheetManager.Domain/Characters/Character.cs
@@ -0,0 +1,20 @@
+﻿namespace RPGSheetManager.Domain.Characters {
+    public class Character {
+        public Guid Id { get; set; }
+        public Guid SystemId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public List<CharacterData> Data { get; set; }
+    }
+
+    public class CharacterData {
+        public string Name { get; set; }
+        public string Component { get; set; }
+        public string Value { get; set; }
+        public bool Editable { get; set; }
+        public bool SessionEditable { get; set; }
+        public bool Visible { get; set; }
+        public string Category { get; set; }
+    }
+}
